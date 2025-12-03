69edcc28054fda4af96e228ed2ccf4530193f418
Alex <jinghui.huang@spacemit.com>
2025-12-03 17:39:57 +0800

    add fp16 legalize

--- xslim/quantizer/convert_to_fp16.py
+++ xslim/quantizer/convert_to_fp16.py
@@ -9,13 +9,48 @@ from xslim.logger import logger
 import onnx_graphsurgeon as osg
 from ..onnx_graph_helper import format_onnx_model
 
-def legalize_fp16_graph(osg_graph : osg.Graph):
+
+def legalize_fp16_graph(osg_graph: osg.Graph):
     for node in osg_graph.nodes:
         if node.op in {"Resize", "Upsample"}:
             for input_var in node.inputs:
                 if isinstance(input_var, osg.Constant) and input_var.dtype == np.float16:
                     input_var.values = input_var.values.astype(np.float32)
+        elif node.op in {"Cast"}:
+            to_dtype = onnx.helper.tensor_dtype_to_np_dtype(node.attrs["to"])
+            if node.outputs[0].dtype != to_dtype:
+                node.attrs["to"] = onnx.helper.np_dtype_to_tensor_dtype(
+                    node.outputs[0].dtype)
+        elif node.op in {"Equal", "NotEqual", "Greater", "Less", "GreaterEqual", "LessEqual", "Add", "Sub", "Mul", "Div"}:
+            if node.inputs[0].dtype == np.float16 and node.inputs[1].dtype != np.float16:
+                if isinstance(node.inputs[1], osg.Constant):
+                    node.inputs[1].values = node.inputs[1].values.astype(
+                        np.float16)
+                else:
+                    raise RuntimeError("Unsupported op {} with fp16 inputs".format(node.op))
+            elif node.inputs[0].dtype != np.float16 and node.inputs[1].dtype == np.float16:
+                if isinstance(node.inputs[0], osg.Constant):
+                    node.inputs[0].values = node.inputs[0].values.astype(
+                        np.float16)
+                else:
+                    raise RuntimeError("Unsupported op {} with fp16 inputs".format(node.op))
+        elif node.op in {"Range"}:
+            remove_var = []
+            add_var = []
+            remove_idx = []
+            for input_var in node.inputs:
+                if isinstance(input_var, osg.Constant) and input_var.dtype == np.float16:
+                    new_var = osg.Constant("{}_to_fp32".format(
+                        input_var.name), input_var.values.astype(np.float32))
+                    remove_idx.append(node.inputs.index(input_var))
+                    remove_var.append(input_var)
+                    add_var.append(new_var)
+
+            for r_var, add_var, r_idx in zip(remove_var, add_var, remove_idx):
+                node.inputs[r_idx] = add_var
+
     return osg_graph
+
 
 def convert_to_fp16_onnx_model(
     file_or_model: Union[str, onnx.ModelProto],
@@ -34,12 +69,40 @@ def convert_to_fp16_onnx_model(
     model_opt = format_onnx_model(onnx_model, sim_en)
 
     logger.info("convert onnx model to fp16.")
+
+    default_ignore_op_types = {"ArrayFeatureExtractor",
+                               "Binarizer",
+                               "CastMap",
+                               "CategoryMapper",
+                               "DictVectorizer",
+                               "FeatureVectorizer",
+                               "Imputer",
+                               "LabelEncoder",
+                               "LinearClassifier",
+                               "LinearRegressor",
+                               "Normalizer",
+                               "OneHotEncoder",
+                               "RandomUniformLike",
+                               "RandomNormalLike",
+                               "SVMClassifier",
+                               "SVMRegressor",
+                               "Scaler",
+                               "TreeEnsembleClassifier",
+                               "TreeEnsembleRegressor",
+                               "ZipMap",
+                               "NonMaxSuppression",
+                               "TopK",
+                               "RoiAlign",
+                               "Range",
+                               "CumSum"}
+
     try:
         model_fp16 = convert_float_to_float16.convert_float_to_float16(
             model_opt,
             keep_io_types=True,
             disable_shape_infer=False,
-            op_block_list=ignore_op_types_list,
+            op_block_list=list(
+                default_ignore_op_types or set(ignore_op_types_list)),
             node_block_list=ignore_node_names_list,
         )
     except Exception as e:
